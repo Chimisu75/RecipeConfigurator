a72a00469bd6ec25a677c270ee03e28f29266699
Chimisu75 <chems.dabbabi@gmail.com>
2024-05-29 12:05:53 +0200

    Interaction RecipeStep

--- RecipeConfigurator/src/App.jsx
+++ RecipeConfigurator/src/App.jsx
@@ -1,37 +1,54 @@
-import RecipeAjout from "./components/RecipeAjoutEtapes";
 import RecipePhoto from "./components/RecipePhoto";
 import RecipeStep from "./components/RecipeStep";
 import RecipeIngredient from "./components/RecipeIngredient";
 import RecipeSubmitBtn from "./components/RecipeSubmitBtn";
 import RecipeName from "./components/RecipeName";
 import RecipeNbPeople from "./components/RecipeNbPeople";
-import RecipeDifficulty from "./components/RecipeDifficulty";
-import RecipeAjoutEtapes from "./components/RecipeAjoutEtapes";
+
 import RecipeAjoutIngredients from "./components/RecipeAjoutIngredients";
 import "./index.css";
+import { useState } from "react";
 
 function App() {
+  const [recipeSteps, setRecipeSteps] = useState([]);
+
+  const handleAddRecipeStep = () => {
+    const stepIndex = recipeSteps.length + 1;
+    setRecipeSteps((prevSteps) => [
+      ...prevSteps,
+      <RecipeStep key={stepIndex} onAddStep={handleAddRecipeStep} />,
+    ]);
+  };
   return (
     <>
-  <h1>Créer votre recette</h1>
-  <div className="container">
-    <section className="section1">
-      <RecipeName />
-      <h2 className="recipe-subtitle">RECETTE :</h2>
-      <RecipeStep />
-      <RecipeStep />
-      <RecipeAjoutEtapes />
-    </section>
-    <section className="section2">
-      <RecipePhoto />
-      <RecipeNbPeople />
-      <h2 className="recipe-subtitle">INGREDIENTS : </h2>
-      <RecipeIngredient />
-      <RecipeAjoutIngredients />
-    </section>
-  </div>
-  <RecipeSubmitBtn />
-</>
+      <h1>Créer votre recette</h1>
+      <div className="container">
+        <section className="section1">
+          <RecipeName />
+          <h2 className="recipe-subtitle">RECETTE :</h2>
+          {recipeSteps.map((step) => step)}
+          <RecipeStep onAddStep={handleAddRecipeStep} />
+          <div className="recipe-btn">
+            <button
+              className="recipe-btn-etape"
+              type="button"
+              onClick={handleAddRecipeStep}
+            >
+              + Ajouter une étape
+            </button>
+          </div>
+        </section>
+
+        <section className="section2">
+          <RecipePhoto />
+          <RecipeNbPeople />
+          <h2 className="recipe-subtitle">INGREDIENTS : </h2>
+          <RecipeIngredient />
+          <RecipeAjoutIngredients />
+        </section>
+      </div>
+      <RecipeSubmitBtn />
+    </>
   );
 }
 
--- RecipeConfigurator/src/components/RecipeAjoutEtapes.jsx
+++
@@ -1,9 +0,0 @@
-const RecipeAjoutEtapes = () =>{
-    return(
-        <div className="recipe-btn">
-            <input className="recipe-btn-etape" type="button" value="+ Ajouter une étape"/>
-        </div>
-    );
-};
-
-export default RecipeAjoutEtapes;
