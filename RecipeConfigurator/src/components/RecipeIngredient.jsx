f574a0f695259d69f2e16c73b83e51b438f28980
Emilie <emilie.caverne@outlook.fr>
2024-05-29 17:01:26 +0200

    css x

--- RecipeConfigurator/src/components/RecipeIngredient.jsx
+++ RecipeConfigurator/src/components/RecipeIngredient.jsx
@@ -2,10 +2,13 @@ function RecipeIngredient() {
   return (
     <div className="recipe-ingredient-container">
       <label className="recipe-ingredient-label">
+        <div className="recipe-textarea-btn">
         <textarea
           className="recipe-textarea"
           placeholder="Ajouter un ingrédient"
         ></textarea>
+        <button className="recipe-btn-ingredient-delete">x</button>
+        </div>
         <div className="recipe-controls">
         <input type="number" className="recipe-ingredient-cost" placeholder="Coût"/>
         </div>
